3ac3310cea5352aab0b63bf83c33bbe86f1ef900
Hyeonseo Kim <dodok8@gmail.com>
2021-07-26 02:42:56 +0900

    Ch 3.3.2 ~ 3.3.3 props 값 지정하기

--- 03/hello-react/src/MyComponent.tsx
+++ 03/hello-react/src/MyComponent.tsx
@@ -1,5 +1,9 @@
-function MyComponent() {
-  return <div>나의 새롭고 멋진 컴포넌트</div>;
+function MyComponent(props: { name: string }) {
+  return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
 }
+
+MyComponent.defaultProps = {
+  name: '기본 이름',
+};
 
 export default MyComponent;
