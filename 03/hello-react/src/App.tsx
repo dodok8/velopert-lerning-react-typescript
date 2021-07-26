c428fb3e462d5e5e8668bb0f9e7fea3a9694b9a9
Hyeonseo Kim <dodok8@gmail.com>
2021-07-26 16:45:11 +0900

    비구조화 할당과 TS를 이용한 정리

--- 03/hello-react/src/App.tsx
+++ 03/hello-react/src/App.tsx
@@ -1,7 +1,7 @@
 import MyComponent from './MyComponent';
 
 function App() {
-  return <MyComponent name="React">리액트</MyComponent>;
+  return <MyComponent name="React" favoriteNumber={3}>리액트</MyComponent>;
 }
 
 export default App;
--- 03/hello-react/src/MyComponent.tsx
+++ 03/hello-react/src/MyComponent.tsx
@@ -1,8 +1,34 @@
 import PropTypes from 'prop-types';
 
-function MyComponent(props: any) {
+// function MyComponent(props: any) {
+//   const { name, children, favoriteNumber } = props;
+//   return (
+//     <div>
+//       안녕하세요, 제 이름은 {name}입니다.
+//       <br />
+//       children 값은 {children}입니다.
+//       <br />
+//       제가 쫗아하는 숫자는 {favoriteNumber} 입니다.
+//     </div>
+//   );
+// }
+
+// MyComponent.defaultProps = {
+//   name: '기본 이름',
+// };
+
+// MyComponent.propTypes = {
+//   name: PropTypes.string,
+//   children: PropTypes.string,
+//   favoriteNumber:PropTypes.number.isRequired
+// }
+
+// favoriteNumber에 기본 값을 넣어줘야 하는 것 빼면 거의 똑같이 작동하는 코드
+type MyComponentProps = { name?: string, children?: string, favoriteNumber: number }
+
+function MyComponent(props: MyComponentProps = {name: "기본 이름", favoriteNumber: 1}) {
   const { name, children, favoriteNumber } = props;
-  return (
+    return (
     <div>
       안녕하세요, 제 이름은 {name}입니다.
       <br />
@@ -11,15 +37,6 @@ function MyComponent(props: any) {
       제가 쫗아하는 숫자는 {favoriteNumber} 입니다.
     </div>
   );
-}
-
-MyComponent.defaultProps = {
-  name: '기본 이름',
-};
-
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favoriteNumber:PropTypes.number.isRequired
 }
 
 export default MyComponent;
