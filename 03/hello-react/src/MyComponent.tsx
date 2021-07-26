0ab41104fbf40c833fd30f32e5f1736084a881f0
Hyeonseo Kim <dodok8@gmail.com>
2021-07-26 16:29:05 +0900

    ch 3.3.6 propTypes를 통한 검증

--- 03/hello-react/src/MyComponent.tsx
+++ 03/hello-react/src/MyComponent.tsx
@@ -1,10 +1,14 @@
-function MyComponent(props: { name: string; children: string }) {
-  const { name, children } = props;
+import PropTypes from 'prop-types';
+
+function MyComponent(props: any) {
+  const { name, children, favoriteNumber } = props;
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다.
       <br />
       children 값은 {children}입니다.
+      <br />
+      제가 쫗아하는 숫자는 {favoriteNumber} 입니다.
     </div>
   );
 }
@@ -12,5 +16,10 @@ function MyComponent(props: { name: string; children: string }) {
 MyComponent.defaultProps = {
   name: '기본 이름',
 };
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber:PropTypes.number.isRequired
+}
 
 export default MyComponent;
