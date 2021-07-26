98c3dc98f039407a0440a1128ac5bb21d0a9ef8e
Hyeonseo Kim <dodok8@gmail.com>
2021-07-26 17:28:26 +0900

    ch 3.4 state

--- 03/hello-react/src/App.tsx
+++ 03/hello-react/src/App.tsx
@@ -1,7 +1,8 @@
-import MyComponent from './MyComponent';
+// import MyComponent from './MyComponent';
+import Say from './Say';
 
 function App() {
-  return <MyComponent name="React" favoriteNumber={3}>리액트</MyComponent>;
+  return <Say />;
 }
 
 export default App;
