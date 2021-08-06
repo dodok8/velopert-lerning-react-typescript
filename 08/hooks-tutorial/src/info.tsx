df202234dda9c7bdd129ff939e39b530769bcce9
Hyeonseo Kim <dodok8@gmail.com>
2021-08-06 17:14:38 +0900

    ch 8.3

--- 08/hooks-tutorial/src/info.tsx
+++ 08/hooks-tutorial/src/info.tsx
@@ -1,22 +1,50 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
+
+/*type State = {
+  [key: string]: any;
+};
+이러면 어느 키를 가지던 간에 state에 넣을 수 있지만 좋은 방법은 아니라고 생각합니다. 차라리 any를 쓰지*/
+
+type State = {
+  name: string;
+  nickname: string;
+};
+
+function reducer(state: State, action: EventTarget & HTMLInputElement) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 function Info() {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [{ name, nickname }, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: '',
+  } as State);
 
-  function onChangeName(e: React.ChangeEvent<HTMLInputElement>) {
-    setName(e.target.value);
-  }
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
 
-  function onChangeNickname(e: React.ChangeEvent<HTMLInputElement>) {
-    setNickname(e.target.value);
+  useEffect(() => {
+    console.log('effect');
+    console.log(name);
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    };
+  }, [name]);
+
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    dispatch(e.target);
   }
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
